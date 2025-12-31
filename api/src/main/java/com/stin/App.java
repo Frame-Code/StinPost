b2deeb54d11f8e355870dda18fa0e595f93e20dc
Daniel Mora C. <moraisurcan@hotmail.com>
2025-12-30 22:28:08 -0500

    Arquitecture project initialize

--- api/src/main/java/com/stin/App.java
+++ api/src/main/java/com/stin/App.java
@@ -1,13 +1,13 @@
-package com.stin.demo;
+package com.stin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
